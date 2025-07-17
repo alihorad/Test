7841329da68ec96506e9fbd9673cee4dbb7486b0
a_lihorad <a.lihorad@internet.ru>
2025-07-17 18:14:45 +0300

    First commit

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,36 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+
+
+
+
+
+    //ofstream fileInput("C:\\Users\\User\\Desktop\\outTxt.txt");
+    ofstream fileInput("outTxt.txt");
+
+    if (fileInput.is_open())
+    {
+
+        fileInput << "1`111111";
+
+        fileInput.close();
+    }
+    else
+    {
+        cout << "Ошибка";
+    }
+
+
+
+
+    
+
+}
