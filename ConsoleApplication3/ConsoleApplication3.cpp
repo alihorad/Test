b08da48f768ea27602580f321049a730d68717c6
a_lihorad <a.lihorad@internet.ru>
2025-07-17 18:16:04 +0300

    update commit

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -10,27 +10,4 @@ int main()
     system("chcp 1251");
 
 
-
-
-
-    //ofstream fileInput("C:\\Users\\User\\Desktop\\outTxt.txt");
-    ofstream fileInput("outTxt.txt");
-
-    if (fileInput.is_open())
-    {
-
-        fileInput << "1`111111";
-
-        fileInput.close();
-    }
-    else
-    {
-        cout << "Ошибка";
-    }
-
-
-
-
-    
-
 }
